ac3d0369643b922ea254741532aed9b614f954a9
Inon Sinn <inon.sinn@gmail.com>
2022-04-05 23:14:42 +0300

    finished Matrix creation

--- Matrix.hpp
+++ Matrix.hpp
@@ -2,21 +2,55 @@
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
+#include <vector>
+
 
 namespace zich{
 
     class Matrix
     {
     private:
-        /* data */
+        std::vector<double> array;
+        int rows;
+        int columns;
     public:
-        Matrix(/* args */);
-        ~Matrix();
+        Matrix();
+        Matrix(std::vector<double> arr, int n, int m);
+        ~Matrix() = default;
 
-        Matrix(vector<double> array, unsigned int n, unsigned int m);
 
-        // Plus Operator
+        // Plus and Minus Operators
         Matrix operator+() const ;
+        Matrix operator+(const Matrix& other) const;
+        Matrix& operator+=(const Matrix& other);
+        Matrix operator-() const ;
+        Matrix operator-(const Matrix& other) const;
+        Matrix& operator-=(const Matrix& other);
+
+        // Comparsion operators
+        bool operator>(const Matrix& other) const;
+        bool operator>=(const Matrix& other) const;
+        bool operator<(const Matrix& other) const;
+        bool operator<=(const Matrix& other) const;
+        bool operator==(const Matrix& other) const;
+        bool operator!=(const Matrix& other) const;
+
+        // addition and substition by 1, prefix and postfix
+        Matrix& operator++();
+        Matrix& operator++(int);
+        Matrix& operator--();
+        Matrix& operator--(int);
+
+        // multiplication operator
+        friend Matrix operator*(double real_number,const Matrix& other);
+        Matrix operator*(const Matrix& other) const;
+        Matrix& operator*=(const Matrix& other);
+        Matrix operator*(double real_number) const;
+        Matrix& operator*=(double real_number);
+
+        // input and output oprators
+        friend std::ostream& operator<< (std::ostream& output, const Matrix& other);
+        friend std::istream& operator>> (std::istream& input , Matrix& other);
 
 
 
